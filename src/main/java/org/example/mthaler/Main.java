e1d840ed495342668a5177720f8c263fe3d05afd
Marius Thaler <marius.thaler@outlook.com>
2024-09-30 15:25:09 +0200

    Main Worttrainer added

--- src/main/java/org/example/mthaler/Main.java
+++ src/main/java/org/example/mthaler/Main.java
@@ -1,7 +1,51 @@
 package org.example.mthaler;
 
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import java.awt.Image;
+import java.util.ArrayList;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rechtschreibtrainer trainer = new Rechtschreibtrainer();
+
+
+
+        for(int i = 0; i < 5; i++) {
+            trainer.randomWord();
+
+            for (;;) {
+                // Lade das Bild
+                ImageIcon originalIcon = new ImageIcon(trainer.getCurrentWord().getImageURL());
+
+                // Skaliere das Bild auf die gewünschte Größe (z.B. 100x100 Pixel)
+                Image scaledImage = originalIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+                ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+                // Kombiniere Bild und Text
+                Object[] message = {
+                        "Was siehst du? " + trainer.getCurrentWord().getCorrectAnswers() + "/" + trainer.getCurrentWord().getTotalAnswers() + " richtig",
+                        scaledIcon,
+                };
+
+                // Frage anzeigen
+                String userInput = JOptionPane.showInputDialog(null, message, "Bild, Text und Eingabe", JOptionPane.PLAIN_MESSAGE);
+                if (userInput == null) {
+                    JOptionPane.showMessageDialog(null, "Programm abgebrochen", "Abbruch", JOptionPane.WARNING_MESSAGE);
+                    System.exit(0); // Programm beenden
+                }
+
+                if(trainer.checkWord(userInput)) {
+                    break;
+                }
+            }
+
+        }
+
+        JOptionPane.showMessageDialog(null, "Quiz abgeschlossen\n" + trainer.getCorrectAnswersTotal() + "/5 Fragen richtig", "Fertig", JOptionPane.INFORMATION_MESSAGE);
     }
+
+
 }
