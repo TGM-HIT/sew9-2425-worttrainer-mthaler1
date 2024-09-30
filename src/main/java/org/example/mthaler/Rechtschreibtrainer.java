2453844c0811a15e2deb121c1f2f44bfb95a542c
Marius Thaler <marius.thaler@outlook.com>
2024-09-30 15:28:46 +0200

    Strategy Pattern to Rechtschreibtrainer

--- src/main/java/org/example/mthaler/Rechtschreibtrainer.java
+++ src/main/java/org/example/mthaler/Rechtschreibtrainer.java
@@ -1,4 +1,56 @@
 package org.example.mthaler;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
 public class Rechtschreibtrainer {
+    private ArrayList<Word> words;
+    private Word currentWord;
+    private int currentWordIndex;
+    private int correctAnswersTotal;
+    private ReadWrite storage;
+
+    public Rechtschreibtrainer() {
+        this.storage = new JsonReadWrite();
+        this.words = storage.read();
+    }
+
+    public void addWord(Word word) {
+        this.words.add(word);
+    }
+
+    public void randomWord() {
+        Random random = new Random();
+
+        currentWordIndex = random.nextInt(this.words.size());
+        this.currentWord = this.words.get(currentWordIndex);
+    }
+
+    public boolean checkWord(String eingabe) {
+        currentWord.setTotalAnswers();
+        if(eingabe.equals(this.currentWord.getName())) {
+            currentWord.setCorrectAnswers();
+            this.correctAnswersTotal++;
+            this.saveValues();
+            return true;
+        } else {
+            this.saveValues();
+            return false;
+        }
+    }
+
+    public Word getCurrentWord() {
+        return currentWord;
+    }
+
+    private void saveValues() {
+        this.words.set(currentWordIndex, currentWord);
+        storage.write(this.words);
+    }
+
+    public int getCorrectAnswersTotal() {
+        return correctAnswersTotal;
+    }
 }
+
